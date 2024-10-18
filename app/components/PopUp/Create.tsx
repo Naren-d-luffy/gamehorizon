ca47547a57a0b759c177483d3b5b56ce0dae549e
Naren <deadlyassassin2003@gmail.com>
2024-10-18 18:41:30 +0530

    Creating a Pop-up (Without anything) to Add new Games and integrated it to the Main

---
+++ app/components/PopUp/Create.tsx
@@ -0,0 +1,21 @@
+interface ModuleProps {
+  isOpen: boolean;
+  onClose: () => void;
+}
+
+export default function Create({ isOpen, onClose }: ModuleProps) {
+  if (!isOpen) return null;
+
+  return (
+    <div className="fixed inset-0 bg-gray-800 bg-opacity-75 flex justify-center items-center z-50">
+      <div className="bg-white p-10 rounded-lg overflow-hidden shadow-xl transform transition-all sm:max-w-lg sm:w-full">
+        <div className="flex justify-between">
+          <h1>hello team</h1>
+        <button onClick={onClose} className="bg-blue-500 p-5">X</button>
+        </div>
+        
+        
+      </div>
+    </div>
+  );
+}
--- app/components/main.tsx
+++ app/components/main.tsx
@@ -1,9 +1,17 @@
+"use client"
 import Nav from "./nav";
-import React from "react";
+import React,{useState} from "react";
 import Image from "next/image";
 import bully from "../../public/assets/game/bully.jpg";
+import Create from "./PopUp/Create";
 
 export default function Main() {
+
+  const [isCreated, setIsCreatedOpen] = useState(false);
+
+  const openCreated = () => setIsCreatedOpen(true);
+  const closeCreated = () => setIsCreatedOpen(false);
+
   return (
     <>
       <div className="w-full h-screen ">
@@ -34,7 +42,8 @@ export default function Main() {
             <div className="relative">
               <div className="absolute inset-0 bg-blue-700 rounded-lg"></div>
 
-              <button className="relative text-blue-950  bg-blue-300  text-2xl font-roboto font-bold hover:-translate-y-1 hover:translate-x-1 hover:bg-transparent hover:text-white hover:underline border-blue-900 hover:border-white border-2 p-2 rounded-lg transition-all duration-300 ease-in-out">
+              <button className="relative text-blue-950  bg-blue-300  text-2xl font-roboto font-bold hover:-translate-y-1 hover:translate-x-1 hover:bg-transparent hover:text-white hover:underline border-blue-900 hover:border-white border-2 p-2 rounded-lg transition-all duration-300 ease-in-out"
+              onClick={openCreated}>
                 Add a New Game
               </button>
             </div>
@@ -93,6 +102,10 @@ export default function Main() {
             <div className="relative w-full h-full bg-black  rounded-2xl group hover:-translate-y-2 hover:translate-x-2 transition-all duration-300 ease-in-out hover:shadow-lg"></div>
           </div>
         </div>
+        <Create 
+        isOpen={isCreated}
+        onClose={closeCreated}
+        />
       </div>
     </>
   );
