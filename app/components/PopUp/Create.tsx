8c436fb10a99e94be97c66545b277f1629cb30b6
Naren <deadlyassassin2003@gmail.com>
2024-10-24 18:53:27 +0530

    adding disabled submit button to the create.tsx and have done fetching and rendering in the main.tsx

--- app/components/PopUp/Create.tsx
+++ app/components/PopUp/Create.tsx
@@ -1,6 +1,5 @@
 "use client";
 import { useState } from "react";
-import popup from "../../../public/assets/popup.png"
 
 interface ModuleProps {
   isOpen: boolean;
@@ -10,6 +9,35 @@ interface ModuleProps {
 export default function Create({ isOpen, onClose }: ModuleProps) {
   const [isDragging, setIsDragging] = useState(false);
   const [file, setFile] = useState<File | null>(null);
+  const [base64Image, setBase64Image] = useState<string | null>(null);
+  const [gameName, setGameName] = useState("");
+  const [creator, setCreator] = useState("");
+  const [releaseDate, setReleaseDate] = useState("");
+  const [category, setCategory] = useState("");
+  const [review, setReview] = useState("");
+  const [genres, setGenres] = useState<string[]>([]);
+  const availableGenres = [
+    "Adventure",
+    "Action",
+    "Sports",
+    "Simulation",
+    "Platformer",
+    "RPG",
+    "Fighting",
+    "Racing",
+    "Shooter",
+    "Puzzle",
+    "Casual",
+    "Strategy",
+    "Stealth",
+    "Party",
+    "Action RPG",
+    "Tactical RPG",
+    "Survival",
+    "Battle Royale",
+    "Open world",
+    "2D Game",
+  ];
 
   const handleDragOver = (e: React.DragEvent<HTMLDivElement>) => {
     e.preventDefault();
@@ -24,19 +52,66 @@ export default function Create({ isOpen, onClose }: ModuleProps) {
     e.preventDefault();
     setIsDragging(false);
     const droppedFile = e.dataTransfer.files[0];
+    convertToBase64(droppedFile);
     setFile(droppedFile);
   };
 
   const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const selectedFile = e.target.files?.[0] || null;
-    setFile(selectedFile);
+    if (selectedFile) {
+      convertToBase64(selectedFile);
+      setFile(selectedFile);
+    }
+  };
+
+  const convertToBase64 = (file: File) => {
+    const reader = new FileReader();
+    reader.onloadend = () => {
+      setBase64Image(reader.result as string);
+    };
+    reader.readAsDataURL(file);
+  };
+
+  const toggleGenre = (genre: string) => {
+    if (genres.includes(genre)) {
+      setGenres(genres.filter((g) => g !== genre));
+    } else {
+      setGenres([...genres, genre]);
+    }
+  };
+
+  const isFormValid = () => {
+    return (
+      gameName &&
+      creator &&
+      releaseDate &&
+      review &&
+      file &&
+      genres.length > 0 &&
+      category
+    );
+  };
+
+  const getReviewBgClass = () => {
+    switch (review) {
+      case "Don't Try":
+        return "bg-red-600";
+      case "Neutral":
+        return "bg-yellow-300";
+      case "Positive":
+        return "bg-green-300";
+      case "Overwhelmingly Positive":
+        return "bg-green-600";
+      default:
+        return "";
+    }
   };
 
   if (!isOpen) return null;
 
   return (
     <div className="w-full fixed inset-0 bg-gray-800 bg-opacity-75 flex justify-center items-center z-50">
-      <div className="bg-center bg-no-repeat bg-cover w-1/2 p-10 rounded-lg overflow-hidden shadow-xl transform bg-opacity-50 transition-all" style={{ backgroundImage: `url(/assets/controler2.png)` }}>
+      <div className="bg-center bg-no-repeat bg-cover w-1/2 p-10 rounded-lg overflow-hidden shadow-xl transform bg-opacity-50 transition-all bg-gradient-to-br from-blue-200 to-blue-400">
         <div className="flex justify-between">
           <h1 className="text-2xl font-semibold">Add a new Game</h1>
           <button
@@ -89,6 +164,8 @@ export default function Create({ isOpen, onClose }: ModuleProps) {
               type="text"
               placeholder="Enter Game name..."
               className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-green-500"
+              value={gameName}
+              onChange={(e) => setGameName(e.target.value)}
             />
           </div>
           <div className="relative mb-6 space-y-2 w-1/2">
@@ -97,27 +174,51 @@ export default function Create({ isOpen, onClose }: ModuleProps) {
               type="text"
               placeholder="Enter Created by..."
               className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-green-500"
+              value={creator}
+              onChange={(e) => setCreator(e.target.value)}
             />
           </div>
         </div>
 
         <div className="flex gap-5">
-          <div className="relative mb-6 space-y-2 w-1/2">
+          <div className="relative mb-6 space-y-2 w-1/3">
             <label>Release Date</label>
             <input
               type="date"
               className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-green-500"
+              value={releaseDate}
+              onChange={(e) => setReleaseDate(e.target.value)}
             />
           </div>
-          <div className="relative mb-6 space-y-2 w-1/2">
-            <label htmlFor="review" className="block text-gray-700 mb-1">
+
+          <div className="relative mb-6 space-y-2 w-1/3">
+            <label htmlFor="review" className="block mb-1">
+              Category
+            </label>
+            <select
+              name="review"
+              className={`w-full border text-sm rounded-md p-2 focus:ring focus:ring-blue-200 `}
+              value={category}
+              onChange={(e) => setCategory(e.target.value)}
+            >
+              <option value="">Select Category</option>
+              <option value="Played Game">Played Game</option>
+              <option value="Currently Playing">Currently Playing</option>
+              <option value="Want to Play">Want to Play</option>
+            </select>
+          </div>
+
+          <div className="relative mb-6 space-y-2 w-1/3">
+            <label htmlFor="review" className="block mb-1">
               Review
             </label>
             <select
               name="review"
-              className="w-full border border-gray-300 text-sm rounded-md p-2 focus:ring focus:ring-blue-200"
+              className={`w-full border text-sm rounded-md p-2 focus:ring focus:ring-blue-200 `}
+              value={review}
+              onChange={(e) => setReview(e.target.value)}
             >
-              <option value="">Select Type</option>
+              <option value="">Select Review</option>
               <option value="Don't Try">Don't Try</option>
               <option value="Neutral">Neutral</option>
               <option value="Positive">Positive</option>
@@ -127,40 +228,43 @@ export default function Create({ isOpen, onClose }: ModuleProps) {
             </select>
           </div>
         </div>
+
         <div className="mb-6">
           <label className="text-sm font-medium text-gray-700">Genres</label>
           <div className="grid grid-cols-5 gap-3 mt-2">
-            {[
-              "Adventure",
-              "Action",
-              "Sports",
-              "Simulation",
-              "Platformer",
-              "RPG",
-              "Fighting",
-              "Racing",
-              "Shooter",
-              "Puzzle",
-              "Casual",
-              "Strategy",
-              "Stealth",
-              "Party",
-              "Action RPG",
-              "Tactical RPG",
-              "Survival",
-              "Battle Royale",
-              "Open world",
-              "2D Game",
-            ].map((genre) => (
+            {availableGenres.map((genre) => (
               <div
                 key={genre}
-                className="flex items-center justify-center py-2 px-4 border rounded-md bg-gray-100 hover:bg-blue-100 transition-colors duration-300 text-sm"
+                onClick={() => toggleGenre(genre)}
+                className={`flex items-center justify-center py-2 px-4 border rounded-md text-sm cursor-pointer ${
+                  genres.includes(genre)
+                    ? "bg-blue-300 text-white"
+                    : "bg-gray-100 hover:bg-blue-100 transition-colors duration-300"
+                }`}
               >
+                {genres.includes(genre) && (
+                  <span
+                    onClick={(e) => {
+                      e.stopPropagation();
+                      toggleGenre(genre);
+                    }}
+                    className="mr-1 text-xs cursor-pointer"
+                  ></span>
+                )}
                 {genre}
               </div>
             ))}
           </div>
         </div>
+
+        <button
+          className={`w-full py-2 px-4 rounded-md text-white ${
+            isFormValid() ? "bg-green-500" : "bg-gray-400 cursor-not-allowed"
+          }`}
+          disabled={!isFormValid()}
+        >
+          Submit
+        </button>
       </div>
     </div>
   );
--- app/components/main.tsx
+++ app/components/main.tsx
@@ -1,17 +1,42 @@
-"use client"
+"use client";
 import Nav from "./nav";
-import React,{useState} from "react";
+import React, { useEffect, useState } from "react";
 import Image from "next/image";
 import bully from "../../public/assets/game/bully.jpg";
 import Create from "./PopUp/Create";
+import { ObjectId } from "mongodb";
+
+interface Game {
+  _id: ObjectId;
+  image: string;
+  name: string;
+  created: string;
+  date: string;
+  review: string;
+  genres: string[];
+  color: string;
+}
 
 export default function Main() {
+  const [games, setGames] = useState<Game[]>([]);
 
   const [isCreated, setIsCreatedOpen] = useState(false);
 
   const openCreated = () => setIsCreatedOpen(true);
   const closeCreated = () => setIsCreatedOpen(false);
 
+  useEffect(() => {
+    const fetchgame = async () => {
+      try {
+        const response = await fetch("/api/games");
+        const data = await response.json();
+        setGames(data);
+      } catch (error) {
+        console.error("Error fetching games data:", error);
+      }
+    };
+    fetchgame();
+  }, []);
   return (
     <>
       <div className="w-full h-screen ">
@@ -42,8 +67,10 @@ export default function Main() {
             <div className="relative">
               <div className="absolute inset-0 bg-blue-700 rounded-lg"></div>
 
-              <button className="relative text-blue-950  bg-blue-300  text-2xl font-roboto font-bold hover:-translate-y-1 hover:translate-x-1 hover:bg-transparent hover:text-white hover:underline border-blue-900 hover:border-white border-2 p-2 rounded-lg transition-all duration-300 ease-in-out"
-              onClick={openCreated}>
+              <button
+                className="relative text-blue-950  bg-blue-300  text-2xl font-roboto font-bold hover:-translate-y-1 hover:translate-x-1 hover:bg-transparent hover:text-white hover:underline border-blue-900 hover:border-white border-2 p-2 rounded-lg transition-all duration-300 ease-in-out"
+                onClick={openCreated}
+              >
                 Add a New Game
               </button>
             </div>
@@ -54,36 +81,43 @@ export default function Main() {
 
         <div className="w-full h-3/4 gap-10 flex p-20 pt-0 pb-5">
           <div className="relative w-1/4 h-4/5">
-            <div className="absolute inset-0 bg-teal-500 rounded-2xl"></div>
-            <div className="relative w-full h-full bg-black rounded-2xl group hover:-translate-y-2 hover:translate-x-2 transition-all duration-300 ease-in-out hover:shadow-lg">
-              <Image src={bully} alt="Bully" className="rounded-t-2xl" />
-              <h2 className="text-white text-center font-bold text-xl font-serif p-4 pb-1">
-                Bully - Anniversary Edtion
-              </h2>
-              <h3 className="text-btext font-medium text-center pb-3">
-                By Rockstar Games
-              </h3>
-              <h3 className="text-gray-400 font-medium font-roboto px-4 pb-2">
-                Release Date :{" "}
-                <span className="text-gray-200 font-normal">
-                  {" "}
-                  21 Oct, 2008{" "}
-                </span>
-              </h3>
-              <h3 className="text-gray-400 font-medium px-4 pb-2">
-                Reviews :{" "}
-                <span className="p-1 bg-green-500 text-green-50 rounded-xl">
-                  Very Positive
-                </span>
-              </h3>
+            <div className="absolute inset-0 bg-teal-500 rounded-2xl">
+              {games.map((game) => (
+                <div
+                  className="relative w-full h-full bg-black rounded-2xl group hover:-translate-y-2 hover:translate-x-2 transition-all duration-300 ease-in-out hover:shadow-lg"
+                  key={game._id.toString()}
+                >
+                  <Image src={game.image} alt={game.name} width={400} height={10} className="rounded-t-2xl" />
+                  <h2 className="text-white text-center font-bold text-xl font-serif p-4 pb-1">
+                    {game.name}
+                  </h2>
+                  <h3 className="text-btext font-medium text-center pb-3">
+                    {game.created}
+                  </h3>
+                  <h3 className="text-gray-400 font-medium font-roboto px-4 pb-2">
+                    Release Date :{" "}
+                    <span className="text-gray-200 font-normal">
+                      {" "}
+                      {game.date}{" "}
+                    </span>
+                  </h3>
+                  <h3 className="text-gray-400 font-medium px-4 pb-2">
+                    Reviews :{" "}
+                    <span
+                      className={`p-1.5 text-white bg-${game.color} rounded-xl`}
+                    >
+                      {game.review}
+                    </span>
+                  </h3>
 
-              <h3 className="text-gray-400 font-medium font-roboto px-4 text-justify">
-                Genres :{" "}
-                <span className="text-gray-200 font-normal">
-                  Open world, Action Adventure game, Fighting game, Shooter
-                  Video Game, Nonlinear gameplay{" "}
-                </span>
-              </h3>
+                  <h3 className="text-gray-400 font-medium font-roboto px-4 text-justify">
+                    Genres: {" "}
+                    <span className="text-gray-200 font-normal">
+                      {game.genres.join(", ")}{" "}
+                    </span>
+                  </h3>
+                </div>
+              ))}
             </div>
           </div>
 
@@ -102,10 +136,7 @@ export default function Main() {
             <div className="relative w-full h-full bg-black  rounded-2xl group hover:-translate-y-2 hover:translate-x-2 transition-all duration-300 ease-in-out hover:shadow-lg"></div>
           </div>
         </div>
-        <Create 
-        isOpen={isCreated}
-        onClose={closeCreated}
-        />
+        <Create isOpen={isCreated} onClose={closeCreated} />
       </div>
     </>
   );
