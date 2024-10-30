f7d639384d43ce3351454cdb3e4c0fe6c810d500
Naren <deadlyassassin2003@gmail.com>
2024-10-30 15:59:50 +0530

    Adding Loader to played games

--- app/components/Category/CatLoader.tsx
+++ app/components/Category/CatLoader.tsx
@@ -1,21 +1,8 @@
 export default function CatLoader() {
   return (
     <>
-      <div className="relative w-1/4 h-4/5">
-        <div className="absolute inset-0 bg-teal-500 rounded-2xl"></div>
-        <div className="relative w-full h-full bg-black  rounded-2xl group hover:-translate-y-2 hover:translate-x-2 transition-all duration-300 ease-in-out hover:shadow-lg"></div>
-      </div>
-      <div className="relative w-1/4 h-4/5">
-        <div className="absolute inset-0 bg-teal-500 rounded-2xl"></div>
-        <div className="relative w-full h-full bg-black  rounded-2xl group hover:-translate-y-2 hover:translate-x-2 transition-all duration-300 ease-in-out hover:shadow-lg"></div>
-      </div>
-      <div className="relative w-1/4 h-4/5">
-        <div className="absolute inset-0 bg-teal-500 rounded-2xl"></div>
-        <div className="relative w-full h-full bg-black  rounded-2xl group hover:-translate-y-2 hover:translate-x-2 transition-all duration-300 ease-in-out hover:shadow-lg"></div>
-      </div>
-      <div className="relative w-1/4 h-4/5">
-        <div className="absolute inset-0 bg-teal-500 rounded-2xl"></div>
-        <div className="relative w-full h-full bg-black  rounded-2xl group hover:-translate-y-2 hover:translate-x-2 transition-all duration-300 ease-in-out hover:shadow-lg"></div>
+      <div className="flex w-full h-full">
+        <div className="relative w-full h-full bg-gradient-to-r from-gray-700 via-gray-600 to-gray-700 animate-pulse  rounded-2xl"></div>
       </div>
     </>
   );
--- app/components/Category/Played.tsx
+++ app/components/Category/Played.tsx
@@ -2,6 +2,7 @@
 import React, { useEffect, useState } from "react";
 import Image from "next/image";
 import { ObjectId } from "mongodb";
+import CatLoader from "./CatLoader";
 
 interface Game {
   _id: ObjectId;
@@ -17,9 +18,10 @@ interface Game {
 
 export default function Played() {
   const [games, setGames] = useState<Game[]>([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    const fetchgame = async () => {
+    const fetchGame = async () => {
       try {
         const response = await fetch("/api/games");
         const data = await response.json();
@@ -30,55 +32,61 @@ export default function Played() {
         setGames(playedGames);
       } catch (error) {
         console.error("Error fetching games data:", error);
+      } finally {
+        setLoading(false);
       }
     };
-    fetchgame();
+    fetchGame();
   }, []);
 
   return (
-    <div className="w-full h-screen ">
+    <div className="w-full h-screen">
       <div className="w-full h-3/4 gap-10 flex p-20 pt-0 pb-5">
         <div className="relative w-1/4 h-4/5">
-          {games.map((game) => (
-            <div className="absolute inset-0 bg-teal-500 rounded-2xl">
-              <div
-                className="relative w-full h-full bg-black rounded-2xl group hover:-translate-y-2 hover:translate-x-2 transition-all duration-300 ease-in-out hover:shadow-lg"
-                key={game._id.toString()}
-              >
-                <Image
-                  src={game.image}
-                  alt={game.name}
-                  width={400}
-                  height={10}
-                  className="rounded-t-2xl"
-                />
-                <h2 className="text-white text-center font-bold text-xl font-serif p-4 pb-1">
-                  {game.name}
-                </h2>
-                <h3 className="text-btext font-medium text-center pb-3">
-                  {game.created}
-                </h3>
-                <h3 className="text-gray-400 font-medium font-roboto px-4 pb-2">
-                  Release Date:{" "}
-                  <span className="text-gray-200 font-normal">{game.date}</span>
-                </h3>
-                <h3 className="text-gray-400 font-medium px-4 pb-2">
-                  Reviews:{" "}
-                  <span
-                    className={`p-1.5 text-white bg-${game.color} rounded-xl`}
-                  >
-                    {game.review}
-                  </span>
-                </h3>
-                <h3 className="text-gray-400 font-medium font-roboto px-4 text-justify">
-                  Genres:{" "}
-                  <span className="text-gray-200 font-normal">
-                    {game.genres.join(", ")}
-                  </span>
-                </h3>
-              </div>
-            </div>
-          ))}
+          {loading
+            ? <CatLoader />
+            : games.map((game) => (
+                <div
+                  className="absolute inset-0 bg-teal-500 rounded-2xl"
+                  key={game._id.toString()}
+                >
+                  <div className="relative w-full h-full bg-black rounded-2xl group hover:-translate-y-2 hover:translate-x-2 transition-all duration-300 ease-in-out hover:shadow-lg">
+                    <Image
+                      src={game.image}
+                      alt={game.name}
+                      width={400}
+                      height={10}
+                      className="rounded-t-2xl"
+                    />
+                    <h2 className="text-white text-center font-bold text-xl font-serif p-4 pb-1">
+                      {game.name}
+                    </h2>
+                    <h3 className="text-btext font-medium text-center pb-3">
+                      {game.created}
+                    </h3>
+                    <h3 className="text-gray-400 font-medium font-roboto px-4 pb-2">
+                      Release Date:{" "}
+                      <span className="text-gray-200 font-normal">
+                        {game.date}
+                      </span>
+                    </h3>
+                    <h3 className="text-gray-400 font-medium px-4 pb-2">
+                      Reviews:{" "}
+                      <span
+                        className={`p-1.5 text-white bg-${game.color} rounded-xl`}
+                      >
+                        {game.review}
+                      </span>
+                    </h3>
+                    <h3 className="text-gray-400 font-medium font-roboto px-4 text-justify">
+                      Genres:{" "}
+                      <span className="text-gray-200 font-normal">
+                        {game.genres.join(", ")}
+                      </span>
+                    </h3>
+                  </div>
+                </div>
+              ))}
         </div>
       </div>
     </div>
