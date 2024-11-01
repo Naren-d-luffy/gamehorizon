1265c69fcc021edfed88ff88d79e5158c097abe6
Naren <deadlyassassin2003@gmail.com>
2024-11-01 14:38:29 +0530

    changing the bg color

--- app/components/Category/CatLoader.tsx
+++ app/components/Category/CatLoader.tsx
@@ -2,7 +2,7 @@ export default function CatLoader() {
   return (
     <>
       <div className="flex w-full h-full">
-        <div className="relative w-full h-full bg-gradient-to-r from-gray-700 via-gray-600 to-gray-700 animate-pulse  rounded-2xl"></div>
+        <div className="relative w-full h-full bg-black animate-pulse  rounded-2xl"></div>
       </div>
     </>
   );
